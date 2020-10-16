8e2319b88aa6b4707fd2d81fbdbbe5dbe8f0fb51
Surajit Pradhan <58851816+surajit25@users.noreply.github.com>
2020-10-16 13:06:00 +0530

    Update firstquestion.js

--- firstquestion.js
+++ firstquestion.js
@@ -1,10 +1,13 @@
-const solution=(strin,index)=>{
-   
-    var value=strin[index];
-    var str=strin.slice(0,index)+strin.slice(index+1);
+// write a js program to take a string and remove as desired by user.
 
-     return str;
+const solution=(string,char)=>{
 
+    var index=string.indexOf(char);
+    var newstring=string.slice(0,index)+string.slice(index+1);
+
+    console.log(newstring);
 }
 
-console.log(solution("this is surajit",0));
+var charvalue=prompt();
+
+solution("this is for surajit",charvalue);
